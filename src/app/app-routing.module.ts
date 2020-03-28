d45007f8dcdf098d64cfaacb103169adb9f8d3cc
RahulRaj <rajr97333@gmail.com>
2020-03-28 19:10:43 +0100

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import {StudentComponent} from './student/student.component';
-import {TeacherComponent} from './teacher/teacher.component';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { StudentComponent } from "./student/student.component";
+import { TeacherComponent } from "./teacher/teacher.component";
+import { AdminComponent } from "./admin/admin.component";
 
 const routes: Routes = [
-  { path: '', component: TeacherComponent },
-  { path: 'teacher', component: TeacherComponent },
-  { path: 'student', component: StudentComponent }
+  { path: "", component: TeacherComponent },
+  { path: "teacher", component: TeacherComponent },
+  { path: "student", component: StudentComponent },
+  { path: "admin", component: AdminComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
